fdc2a56de3a6321b46b8bdba8605209ee3c5023f
ragweed1514 <reboot8527@outlook.com>
2025-08-31 00:37:42 +0400

    SubformHome.vb

---
+++ NugetaCrypterGUI/Forms/SubForms/SubformHome.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Windows.Forms
+
+Namespace NugetaCrypterGUI.Forms.SubForms
+    Public Partial Class SubformHome
+        Inherits Form
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub SubformHome_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ControlBox = False
+        End Sub
+    End Class
+End Namespace
