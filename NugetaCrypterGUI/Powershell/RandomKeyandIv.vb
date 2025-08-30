c7bec7f84782263a8ba2548b0c9389355cfb53bb
ragweed1514 <reboot8527@outlook.com>
2025-08-31 00:38:07 +0400

    RandomKeyandIv.vb

---
+++ NugetaCrypterGUI/Powershell/RandomKeyandIv.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Security.Cryptography
+
+Namespace MONSTERMCCrypterGUI.Powershell
+    Friend Class RandomKeyandIv
+        Public Shared Function GenerateRandomBytes(ByVal length As Integer) As Byte()
+            Dim bytes = New Byte(length - 1) {}
+            Using rng = New RNGCryptoServiceProvider()
+                rng.GetBytes(bytes)
+            End Using
+            Return bytes
+        End Function
+    End Class
+End Namespace
